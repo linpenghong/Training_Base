05b5a05f9851287ba08ca97ed85c991f529be016
Mingxu Hu <humingxu@gmail.com>
2015-10-26 21:57:11 +0800

    Learn_HEALPix Renewed

--- Learn_HEALPix/playground/coarseFine.cpp
+++ Learn_HEALPix/playground/coarseFine.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+
+#include "chealpix.h"
+
+#define NSIZE 8
+
 int main()
 {
+    long npix = nside2npix(NSIZE);
+    std::cout << "number of pixels = " << npix << std::endl;
+
+    double vec[3];
+    for (long i = 0; i < npix; i++)
+    {
+        pix2vec_nest(NSIZE, i, vec);
+        std::cout << vec[0] << " "
+                  << vec[1] << " "
+                  << vec[2] << std::endl;
+    }
+
     return 0;
 }
