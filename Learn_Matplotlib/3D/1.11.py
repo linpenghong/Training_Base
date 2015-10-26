802361e767aa42704056aa91ad453d2b42adb4e9
Mingxu Hu <humingxu@gmail.com>
2015-10-26 14:29:42 +0800

    Learn_HEALPix Added

--- Learn_Matplotlib/3D/1.11.py
+++ Learn_Matplotlib/3D/1.11.py
@@ -42,7 +42,7 @@ for point in seperateSphere(1000):
     y.append(point[1])
     z.append(point[2])
 
-fig = figure(figsize = (10, 10), dpi = 800)
+fig = figure(figsize = (10, 10), dpi = 300)
 ax = fig.add_subplot(111, projection = '3d')
 ax.scatter(x, y, z)
 
@@ -52,4 +52,4 @@ ax.set_zlim(-1.5, 1.5)
 
 ax.set_title("Pseudo-Even Sphere Seperation")
 
-savefig("Sphere_Seperation.png", dpi = 800)
+savefig("Sphere_Seperation.png", dpi = 300)
