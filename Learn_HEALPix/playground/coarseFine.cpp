2d2c04cd76d1b1233dbb1a469a9dd44af44c0fd6
Mingxu Hu <humingxu@gmail.com>
2015-10-27 19:26:33 +0800

    Some Modification

--- Learn_HEALPix/playground/coarseFine.cpp
+++ Learn_HEALPix/playground/coarseFine.cpp
@@ -4,18 +4,29 @@
 
 #define NSIZE 8
 
+void displayVec(double* vec)
+{
+    std::cout << vec[0] << " "
+              << vec[1] << " "
+              << vec[2] << std::endl;
+}
+
 int main()
 {
     long npix = nside2npix(NSIZE);
     std::cout << "number of pixels = " << npix << std::endl;
 
+    long ipnest = 500;
+    std::cout << "NESTED index = " << ipnest << std::endl;
+
     double vec[3];
-    for (long i = 0; i < npix; i++)
+    pix2vec_nest(NSIZE, ipnest, vec);
+    displayVec(vec);
+
+    for (long i = ipnest * 4; i < (ipnest + 1) * 4; i++)
     {
-        pix2vec_nest(NSIZE, i, vec);
-        std::cout << vec[0] << " "
-                  << vec[1] << " "
-                  << vec[2] << std::endl;
+        pix2vec_nest(2 * NSIZE, i, vec);
+        displayVec(vec);
     }
 
     return 0;
