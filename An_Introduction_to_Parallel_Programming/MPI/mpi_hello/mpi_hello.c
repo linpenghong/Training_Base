b0a4514467d009ac815b77e4633b6f406e2e14b6
Mingxu Hu <humingxu@gmail.com>
2015-11-06 17:00:07 +0800

    An_Introduction_to_Paralell_Programming Added

---
+++ An_Introduction_to_Parallel_Programming/MPI/mpi_hello/mpi_hello.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+
+const int MAX_STRING = 100;
+
+int main()
+{
+    char greeting[MAX_STRING];
+
+    int comm_sz;
+    // number of processes
+    
+    int my_rank;
+    // my process rank
+
+    MPI_Init(NULL, NULL);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    // In MPI a communicator is a collection of processes that can send messages
+    // to each other.
+    // MPI_COMM_WORLD is a communicator that consists of all of the processes.
+
+    if (my_rank != 0)
+    {
+        sprintf(greeting,
+                "Greeting from process %d of %d!",
+                my_rank,
+                comm_sz);
+        
+        MPI_Send(greeting,
+                 strlen(greeting) + 1,
+                 MPI_CHAR,
+                 0,
+                 0,
+                 MPI_COMM_WORLD);
+    }
+    else
+    {
+        printf("Greeting from process %d of %d!\n",
+               my_rank,
+               comm_sz);
+
+        for (int q = 1; q < comm_sz; q++)
+        {
+            MPI_Recv(greeting,
+                     MAX_STRING,
+                     MPI_CHAR,
+                     q,
+                     0,
+                     MPI_COMM_WORLD,
+                     MPI_STATUS_IGNORE);
+
+            printf("%s\n", greeting);
+        }
+    }
+
+    MPI_Finalize();
+}
