46bff970ca3e453b3fc0735e3c77096a44170562
Mingxu Hu <humingxu@gmail.com>
2015-11-20 00:09:49 +0800

    Regex Upgrade

---
+++ C_Playground/Regex.cpp
@@ -0,0 +1,24 @@
+#include <regex>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const char cstr0[] = "C3";
+    const char cstr1[] = "ABC3";
+    const char cstr2[] = "C3DE";
+
+    regex e("C\\d");
+
+    if (regex_match(cstr0, e))
+        cout << "0: string object matched\n";
+
+    if (regex_match(cstr1, e))
+        cout << "1: string object matched\n";
+
+    if (regex_match(cstr2, e))
+        cout << "2: string object matched\n";
+
+    return 0;
+}
