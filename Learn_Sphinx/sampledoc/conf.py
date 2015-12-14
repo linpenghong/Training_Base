9bbded1996a37f1b288162eb9e0e31753d7a76ed
Mingxu Hu <humingxu@gmail.com>
2015-12-14 16:21:57 +0800

    Some Modification in Learn_Sphinx

--- Learn_Sphinx/sampledoc/conf.py
+++ Learn_Sphinx/sampledoc/conf.py
@@ -20,6 +20,7 @@ import shlex
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('sphinxext'))
 
 # -- General configuration ------------------------------------------------
 
@@ -29,7 +30,7 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['matplotlib.sphinxext.mathmpl']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
