3734fb4e5484b5e7b0cda39c06c4dc856b95330e
Mingxu Hu <humingxu@gmail.com>
2016-03-04 14:57:53 +0800

    Gaussian.cpp Update

--- Learn_GSL/Random/Gaussian.cpp
+++ Learn_GSL/Random/Gaussian.cpp
@@ -11,7 +11,8 @@ static gsl_rng* RANDR = gsl_rng_alloc(RANDT);
 using namespace std;
 
 // #define GSL_RAN_UGAUSSIAN
-#define GSL_RAN_DIR_3D
+// #define GSL_RAN_DIR_3D
+#define GSL_RAN_BIVARIATE_GAUSSIAN
 
 int main()
 {
