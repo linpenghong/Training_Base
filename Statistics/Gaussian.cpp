6c5c0a87f65dbb434ccad23e4e7e47014f64498b
Mingxu Hu <humingxu@gmail.com>
2016-03-14 13:23:38 +0800

    Chol Added

--- Statistics/Gaussian.cpp
+++ Statistics/Gaussian.cpp
@@ -16,9 +16,36 @@ using namespace arma;
 
 int main()
 {
+    FILE* pSrc = fopen("src.txt", "wb");
+
     mat A(N, 2);
     for (int i = 0; i < N; i++)
+    {
         gsl_ran_bivariate_gaussian(RANDR, 1, 2, 0.5, &A(i, 0), &A(i, 1));
+        fprintf(pSrc, "%12f %12f\n", A(i, 0), A(i, 1));
+    }
 
-    cout << cov(A) << endl;
+    fclose(pSrc);
+
+    mat Q = cov(A);
+    cout << "Covariance Matrix:"
+         << endl
+         << Q
+         << endl;
+
+    mat L = chol(Q, "lower");
+    cout << "Cholesky Decomposition of Covariance Matrix:"
+         << endl
+         << L
+         << endl;
+
+    FILE* pDst = fopen("dst.txt", "wb");
+
+    for (int i = 0; i < N; i++)
+    {
+        vec y = L * randn<vec>(2);
+        fprintf(pDst, "%12f %12f\n", y(0), y(1));
+    }
+
+    fclose(pDst);
 }
