ce7e838f861c134f04872bbd3336b7f868f1fa10
Mingxu Hu <humingxu@gmail.com>
2016-05-11 22:28:52 +0800

    Conf_with_Class_Multi_Logger

--- Learn_easylogging/Conf_with_File_Multi_Logger.cpp
+++ Learn_easylogging/Conf_with_File_Multi_Logger.cpp
@@ -6,8 +6,8 @@ int main(int argc, char* argv[])
 {
     el::Loggers::configureFromGlobal("easylogging_multi_logger.conf");
 
-    CLOG(INFO, "LOGGER_A") << "My first info log using default logger";
-    CLOG(INFO, "LOGGER_B") << "My first info log using default logger";
+    CLOG(INFO, "LOGGER_A") << "Logging into LOGGER_A";
+    CLOG(INFO, "LOGGER_B") << "Logging into LOGGER_B";
 
     return 0;
 }
