5ec36b1123b1da9756c4aca973a307e99a435de1
Mingxu Hu <humingxu@gmail.com>
2016-06-22 14:39:10 +0800

    Python_API of Learn_Sqlite3 Done

---
+++ Learn_Sqlite3/Python_API/delete.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+print "Open database successfully"
+
+conn.execute('''CREATE TABLE COMPANY
+                (ID       INT      PRIMARY KEY   NOT NULL,
+                 NAME     TEXT                   NOT NULL,
+                 AGE      INT                    NOT NULL,
+                 ADDRESS  CHAR(50),
+                 SALARY   REAL);''')
+
+print "Table created successfully";
+
+conn.execute("INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) \
+              VALUES (1, 'Paul', 32, 'California', 2000.00);")
+
+conn.execute("INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) \
+              VALUES (2, 'Allen', 25, 'Texas', 1500.00);")
+
+conn.execute("INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) \
+              VALUES (3, 'Teddy', 23, 'Norway', 2000.00);")
+
+conn.execute("INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) \
+              VALUES (4, 'Mark', 25, 'Rich-Mond', 6500.00);")
+
+conn.commit()
+
+print "Records Created Successfully"
+
+conn.execute("DELETE FROM COMPANY WHERE ID = 2;")
+
+conn.commit()
+
+cursor = conn.execute("SELECT ID, NAME, ADDRESS, SALARY FROM COMPANY;")
+
+for row in cursor:
+    print "ID = ", row[0]
+    print "NAME = ", row[1]
+    print "ADDRESS = ", row[2]
+    print "SALARY = ", row[3], "\n"
+
+print "Operation done successfully"
+
+conn.close()
