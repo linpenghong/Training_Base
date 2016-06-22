380d0578ad554b4c7af253f68e722fc56799056e
Mingxu Hu <humingxu@gmail.com>
2016-06-22 14:29:13 +0800

    More in Python_API

--- Learn_Sqlite3/Python_API/create_table.py
+++ Learn_Sqlite3/Python_API/create_table.py
@@ -3,11 +3,11 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 print "Open database successfully"
 
-conn.execute('''create table company
-                (ID       int      primary key   not null,
-                 NAME     text                   not null,
-                 AGE      int                    not null,
-                 ADDRESS  char(50),
+conn.execute('''CREATE TABLE COMPANY
+                (ID       INT      PRIMARY KEY   NOT NULL,
+                 NAME     TEXT                   NOT NULL,
+                 AGE      INT                    NOT NULL,
+                 ADDRESS  CHAR(50),
                  SALARY   REAL);''')
 
 print "Table created successfully";
