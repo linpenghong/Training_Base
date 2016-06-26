cf68d57e7f1b714b050af4700936e2b9af4d9f86
Mingxu Hu <humingxu@gmail.com>
2016-06-26 12:18:36 +0800

    Done in Gen.py

--- Learn_Sqlite3/Python_STAR/gen.py
+++ Learn_Sqlite3/Python_STAR/gen.py
@@ -22,3 +22,62 @@ imageName = [line.strip().split()[9] for line in lines]
 
 micrographName = [line.strip().split()[13] for line in lines]
 # print micrographName
+
+conn = sqlite3.connect("C15.db")
+
+print "Open Database Successfully"
+
+conn.execute('''CREATE TABLE micrographs
+                (ID             INT     PRIMARY KEY,
+                 Name           TEXT,
+                 Voltage        REAL                  NOT NULL,
+                 DefocusU       REAL                  NOT NULL,
+                 DefocusV       REAL                  NOT NULL,
+                 DefocusAngle   REAL                  NOT NULL,
+                 Cs             REAL                  NOT NULL);''')
+
+conn.execute('''CREATE TABLE particles
+                (ID             INT     PRIMARY KEY,
+                 Name           TEXT,
+                 GroupID        INT                   NOT NULL,
+                 MicrographID   INT                   NOT NULL);''')
+
+conn.execute('''CREATE TABLE groups
+                (ID             INT     PRIMARY KEY,
+                 Name           TEXT);''')
+
+conn.commit()
+
+print "Tables Created"
+
+micrographList = []
+
+for i in range(len(lines)):
+    micrographID = 1
+    if micrographName[i] not in micrographList:
+        micrographList.append(micrographName[i])
+        sql = "INSERT INTO micrographs (Name, Voltage, DefocusU, \
+               DefocusV, DefocusAngle, Cs) VALUES ('" \
+            + micrographName[i] + "', " \
+            + str(voltage[i]) + ", " \
+            + str(defocusU[i]) + ", " \
+            + str(defocusV[i]) + ", " \
+            + str(defocusTheta[i]) + ", " + "0);"
+        conn.execute(sql)
+        conn.commit()
+        micrographID = len(micrographList)
+    else:
+        micrographID = micrographList.index(micrographName[i]) + 1
+
+    groupID = micrographID
+
+    sql = "INSERT INTO particles (Name, GroupID, MicrographID) VALUES ('" \
+        + imageName[i] + "', " \
+        + str(groupID) + ", " \
+        + str(micrographID) +");";
+    conn.execute(sql)
+    conn.commit()
+
+#print micrographList
+
+print "There is ", len(micrographList), "micrographs in total."
